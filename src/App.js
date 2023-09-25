871b6037e5721a70ccb6fa74428d5a5b3147fec3
pasindu997 <pasindunelushan9@gmail.com>
2023-09-25 14:53:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import backgroundImage from "./assets/HomePageImage.jpg";
+import HanthanaGuideImage from"./assets/Hanthana.jpg";
+import UniversityVisit from"./assets/UniversityVisit.jpg";
+import GalBangollow from"./assets/GalBangollow.jpg";
+import UniversityPhotography from "./assets/UniversityPhotography.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="Navbar">
+        <h1 className="heading">eRESERVE</h1>
+        <div className="linknav">
+          <a className="link" href="#">
+            Home
+          </a>
+          <a className="link" href="#">
+            about us
+          </a>
+          <a className="link" href="#">
+            services
+          </a>
+          <a className="link" href="#">
+            contact us
+          </a>
+          <a className="link" href="#">
+            sign in
+          </a>
+          <button class="button button01">sign up</button>
+        </div>
+      </nav>
+      <div className="ImageSize">
+        <img className="backgroundimage" src={backgroundImage} alt="image" />
+      </div>
+      <p>
+        The University of Peradeniya (Sinhala: පේරාදෙණිය විශ්වවිද්‍යාලය, Tamil:
+        பேராதனைப் பல்கலைக்கழகம்) is a public university in Sri Lanka, funded by
+        the University Grants Commission.[2] It is the largest university in Sri
+        Lanka, which was originally established as the University of Ceylon in
+        1942. The university was officially opened on 20 April 1954, in the
+        presence of Queen Elizabeth II, by Prince Philip, Duke of Edinburgh. The
+        University of Peradeniya hosts nine faculties, four postgraduate
+        institutes (including the newly added Postgraduate Institute of Medical
+        Sciences), 20 centres and units, 73 departments, and teaches about
+        12,000 students in the fields of Medicine, Agriculture, Arts, Science,
+        Engineering, Dental Sciences, Veterinary Medicine and Animal Science,
+        Management, and Allied Health Science.[3] It claims to have the largest
+        government endowment by a higher education institution in Sri Lanka,
+        based on its large staff and faculties/departments.
+      </p>
+      <div className="Type">
+        <div className="HantanaGuide">
+        <img className="HanthanaGuideImage" src={HanthanaGuideImage} alt="image" />
+        <button class="button button02">HantanaGuide</button>
+        </div>
+        <div className="UniversityVisit">
+        <img className="UniversityVisit" src={UniversityVisit} alt="image" />
+        </div>
+        <div className="BangollowBooking">
+        <img className="GalBangollow" src={GalBangollow} alt="image" />
+        </div>
+        <div className="Photography">
+        <img className="UniversityPhotography" src={UniversityPhotography} alt="image" />
+        </div>
+        <div className="HostelRegistation"></div>
+      </div>
     </div>
   );
 }
